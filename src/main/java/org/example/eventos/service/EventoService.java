1b1b2db18562554ee04cbf3ea24d1bfbddfbdc54
Pingu <felipe_guaresi@estudante.sesisenai.org.br>
2026-05-19 21:04:24 -0300

    Add files via upload

--- src/main/java/org/example/eventos/service/EventoService.java
+++ src/main/java/org/example/eventos/service/EventoService.java
@@ -1,4 +1,12 @@
 package org.example.eventos.service;
 
+import org.example.eventos.repository.EventoRepository;
+
 public class EventoService {
+
+    private EventoRepository repository;
+
+    public EventoService(EventoRepository repository) {
+        this.repository = repository;
+    }
 }
